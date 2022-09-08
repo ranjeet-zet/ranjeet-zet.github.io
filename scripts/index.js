85604b77aae91fc8fef511d75fcc1facb8c249fc
RANJEET KUMAR <ranjeet030200@gmail.com>
2022-09-09 00:36:45 +0530

    First

---
+++ scripts/index.js
@@ -0,0 +1,19 @@
+var typing = new Typed("#slide-text", {
+  strings: [
+    "Java developer.",
+    "Java Backend developer.",
+    "JavaScript developer.",
+    "Frontend developer.",
+  ],
+  typeSpeed: 30,
+  backSpeed: 40,
+  loop: true,
+});
+
+// dark mode
+// document.querySelector("button").addEventListener("click", function () {
+//   document.documentElement.style.setProperty("--red", "black");
+//   document.documentElement.style.setProperty("--black", "white");
+//   document.querySelector("#header").style.background = "white";
+//   //   alert("Done");
+// });
