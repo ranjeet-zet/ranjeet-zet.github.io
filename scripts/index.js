8df97af4fba4e2967e0de8b07f5ca0e6e6807c21
RANJEET KUMAR <ranjeet030200@gmail.com>
2022-09-09 10:34:51 +0530

    Dark Mode

--- scripts/index.js
+++ scripts/index.js
@@ -17,3 +17,49 @@ var typing = new Typed("#slide-text", {
 //   document.querySelector("#header").style.background = "white";
 //   //   alert("Done");
 // });
+
+let aa = true;
+const toggle = document.querySelector(".toggle");
+const hours = new Date().getHours();
+toggle.checked = hours > 7 && hours < 20;
+if (hours > 7 && hours < 20) {
+  aa = true;
+} else {
+  aa = false;
+}
+
+if (aa) {
+  document.getElementById("header").style.transition = "2s";
+  document.querySelector("#header").style.background =
+    "url(../image/home-day.jpg)";
+  document.getElementById("header").style.backgroundSize = "cover";
+  document.documentElement.style.setProperty("--white", "#080808");
+  aa = false;
+} else {
+  document.getElementById("header").style.transition = "2s";
+  document.querySelector("#header").style.background = "url(../image/home.jpg)";
+  document.getElementById("header").style.backgroundSize = "cover";
+  document.documentElement.style.setProperty("--white", "#fff");
+  aa = true;
+}
+
+toggle.addEventListener("change", function () {
+  if (aa) {
+    document.getElementById("header").style.transition = "2s";
+    document.querySelector("#header").style.background =
+      "url(../image/home-day.jpg)";
+    document.getElementById("header").style.backgroundSize = "cover";
+    // document.querySelector("#header").style.background = "white";
+    document.documentElement.style.setProperty("--white", "#080808");
+    aa = false;
+  } else {
+    document.getElementById("header").style.transition = "2s";
+    document.querySelector("#header").style.background =
+      "url(../image/home.jpg)";
+
+    document.getElementById("header").style.backgroundSize = "cover";
+    // document.getElementById("#header").style.backgroundSize = "cover";
+    document.documentElement.style.setProperty("--white", "#fff");
+    aa = true;
+  }
+});
