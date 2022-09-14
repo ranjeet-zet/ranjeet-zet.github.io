617f1c1edb1b3bf191d9d2cc58a07165204cdc5e
RANJEET KUMAR <ranjeet030200@gmail.com>
2022-09-15 03:01:27 +0530

    Major Update

--- scripts/index.js
+++ scripts/index.js
@@ -119,3 +119,12 @@ document.getElementById("chat-boat").addEventListener("click", function () {
 function headerrefer() {
   window.location.reload();
 }
+
+///////////////////////
+var sidemenu = document.getElementById("sidemenu");
+function openmenu() {
+  sidemenu.style.right = "0";
+}
+function closemenu() {
+  sidemenu.style.right = "-200px";
+}
