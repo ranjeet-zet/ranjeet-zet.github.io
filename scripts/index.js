98723a16d51cea0a99462bfe3a0700f2fffe2a2d
RANJEET KUMAR <ranjeet030200@gmail.com>
2022-09-18 01:20:20 +0530

    Done

--- scripts/index.js
+++ scripts/index.js
@@ -45,7 +45,7 @@ function light() {
   document.getElementById("header").style.transition = "2s";
   document.querySelector("body").style.background = "#fff";
   // document.querySelector("#header").style.background =
-  //   imagearr[1] || "url(../image/white.jpg)";
+  //   imagearr[1] || "url(../image/home-day.jpg)";
   document.getElementById("header").style.backgroundSize = "cover";
   // document.querySelector("#header").style.background = "white";
   document.documentElement.style.setProperty("--white", "#080808");
@@ -59,7 +59,7 @@ function dark() {
   document.querySelector("body").style.background = "#080808";
   document.getElementById("header").style.transition = "2s";
   // document.querySelector("#header").style.background =
-  //   imagearr[0] || "url(../image/black.jpg)";
+  //   imagearr[0] || "url(../image/home.jpg)";
 
   document.getElementById("header").style.backgroundSize = "cover";
   // document.getElementById("#header").style.backgroundSize = "cover";
